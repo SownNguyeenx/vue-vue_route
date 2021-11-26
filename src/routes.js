a73355ec2e66ff374f74ce19b934e93e453dc13b
SownNguyeenx <sonnguyen201298@gmail.com>
2021-11-26 15:00:18 +0700

    4

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,29 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import About from './components/About.vue'
-import Home from './components/HelloWorld.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import About from "./components/About.vue";
+import Home from "./components/HelloWorld.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 export const router = new VueRouter({
-    mode: 'history',
-    routes: [
-        {path: '/', component: Home},
-        {path: '/about', component: About}
-    ]
-})
+  mode: "history",
+  routes: [
+    { path: "/", component: Home },
+    { path: "/login", component: About },
+
+    // { path: '/login', component: Login },
+    { path: "*", redirect: "/" },
+  ],
+});
+
+router.beforeEach((to, from, next) => {
+  const publicPage = ["/login", "/register"];
+  const authRequired = !publicPage.includes(to.path);
+  const loggedIn = localStorage.getItem("user");
+
+  if (authRequired && !loggedIn) {
+    return next("/login");
+  }
+
+  next();
+});
